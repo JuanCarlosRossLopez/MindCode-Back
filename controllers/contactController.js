33a998bc1d8e2ef4a59cc44ab0a9cc5a57861756
JuanRossLopez <rosslopezjuancarlos18@gmail.com>
2025-07-05 23:53:26 -0500

    correción de errores en el contacto

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,17 +5,49 @@ const Contacto = getContactoModel();
 
 exports.create = async (req, res) => {
   try {
-    const contacto = await Contacto.create(req.body);
+    const {
+  nombre_completo,
+  correo,
+  telefono,
+  mensaje,
+  Cursos_id
+} = req.body.mailData || {};
+
+
+    console.log('Datos recibidos en backend:', req.body);
+
+    // Validación básica
+    if (!nombre_completo || !correo || !telefono || !mensaje) {
+  return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+}
+
+    const contacto = await Contacto.create({
+      nombre_completo,
+      correo,
+      telefono,
+      mensaje,
+      Cursos_id: Cursos_id || null
+    });
 
     // Enviar correo
-    await sendConfirmationMail(req.body);
+    await sendConfirmationMail({
+  nombre_completo,
+  correo,
+  mensaje
+});
+
+
 
     res.status(201).json({ message: 'Contacto creado y correo enviado', contacto });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Error al crear contacto o enviar correo' });
+    res.status(500).json({
+      message: 'Error al crear contacto o enviar correo',
+      detalles: err.errors || err.message
+    });
   }
 };
+
 
 exports.findAll = async (req, res) => {
   try {
